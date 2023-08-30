13bd215d2353e3839e77e125e678af9d292cc103
Raí <megac154@gmail.com>
2023-08-30 20:18:52 -0300

    refactor css Formulario

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Botao from '../Botao'
-import './style.scss'
+import style from './Formulario.module.scss'
 
 export default function Formulario() {
   return (
-    <form className='novaTarefa'>
-      <div className='inputContainer'>
+    <form className={style.novaTarefa}>
+      <div className={style.inputContainer}>
         <label htmlFor='tarefa'>
           Adicione um novo estudo
         </label>
@@ -17,7 +17,7 @@ export default function Formulario() {
           required
         />
       </div>
-      <div className='inputContainer'>
+      <div className={style.inputContainer}>
         <label htmlFor='tempo'>
           Tempo
         </label>
