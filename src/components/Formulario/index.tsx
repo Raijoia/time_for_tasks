4e364e94c7f56f7d5276158df9a6e1b62da8c803
Raí <megac154@gmail.com>
2023-08-31 15:27:07 -0300

    state from

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,38 +2,57 @@ import React from 'react'
 import Botao from '../Botao'
 import style from './Formulario.module.scss'
 
-export default function Formulario() {
-  return (
-    <form className={style.novaTarefa}>
-      <div className={style.inputContainer}>
-        <label htmlFor='tarefa'>
-          Adicione um novo estudo
-        </label>
-        <input 
-          type='text'
-          name='tarefa'
-          id='tarefa'
-          placeholder='O que você quer estudar'
-          required
-        />
-      </div>
-      <div className={style.inputContainer}>
-        <label htmlFor='tempo'>
-          Tempo
-        </label>
-        <input 
-          type='time'
-          step='1'
-          name='tempo'
-          id='tempo'
-          min='00:00:00'
-          max='01:30:00'
-          required
-        />
-      </div>
-      <Botao>
-        Adicionar
-      </Botao>
-    </form>
-  )
+export default class Formulario extends React.Component {
+
+  state = {
+    tarefa: '',
+    tempo: '00:00'
+  }
+
+  adicionarTarefa(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log('state: ', this.state)
+  }
+
+  render(){
+      return (
+        // bind(this) => para a função pegar o escopo do this
+        <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
+          <div className={style.inputContainer}>
+            <label htmlFor='tarefa'>
+              Adicione um novo estudo
+            </label>
+            <input 
+              type='text'
+              name='tarefa'
+              value={this.state.tarefa}
+              onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value })}
+              id='tarefa'
+              placeholder='O que você quer estudar'
+              required
+            />
+          </div>
+          <div className={style.inputContainer}>
+            <label htmlFor='tempo'>
+              Tempo
+            </label>
+            <input 
+              type='time'
+              step='1'
+              name='tempo'
+              value={this.state.tempo}
+              onChange={evento => this.setState({ ...this.state , tempo: evento.target.value })}
+              id='tempo'
+              min='00:00:00'
+              max='01:30:00'
+              required
+            />
+          </div>
+          <Botao>
+            Adicionar
+          </Botao>
+        </form>
+      )
+  }
+
 }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Lista.module.scss'
 import Item from './Item'
 
 export default function Lista() {
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     {
       tarefa: 'React',
       tempo: '02:00:00'
@@ -13,11 +13,15 @@ export default function Lista() {
     }, {
       tarefa: 'TypeScript',
       tempo: '03:00:00'
-    }
-  ]
+    },
+  ])
+
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+        console.log(tarefas)
+        setTarefas([...tarefas, { tarefa: "Estudy", tempo:"05:00:00" }])
+      }} >Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 
