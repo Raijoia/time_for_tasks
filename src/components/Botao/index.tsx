9fdc499dfbff152e880e64786870c69b284255c8
Raí <megac154@gmail.com>
2023-08-31 20:35:10 -0300

    regressiva implement

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import style from './Botao.module.scss'
 
 // colocando prop no modo de class, ja colocando o type da props
-class Botao extends React.Component<{ children: string, type?: "button" | "submit" | "reset" | undefined }> {
+class Botao extends React.Component<{ children: string, type?: "button" | "submit" | "reset" | undefined, onClick?: () => void }> {
   render() {
-    const { type = 'button' } = this.props
+    const { type = 'button', onClick } = this.props
     return (
-      <button className={style.botao} type={type}>
+      <button className={style.botao} type={type} onClick={onClick}>
         {this.props.children}
       </button>
     )
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -4,6 +4,7 @@ import Relogio from './Relogio'
 import style from './Cronometro.module.scss'
 import { tempoParaSegundos } from '../../common/utils/time'
 import { ITarefa } from '../../types/tarefa'
+import { setTimeout } from 'timers/promises'
 
 interface Props {
   selecionado: ITarefa | undefined
@@ -19,13 +20,25 @@ export default function Cronometro({ selecionado }: Props) {
     }
   }, [selecionado])
 
+  // tipando o contador e falando que se caso for undefined, valor dele será 0
+  function regressiva(contador: number = 0) {
+    // setando um tempo para a função ser iniciada
+    window.setTimeout(() => {
+      // fazendo um loop
+      if(contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio tempo={tempo}/>
       </div>
-      <Botao>
+      <Botao onClick={() => regressiva(tempo)}>
         Começar!
       </Botao>
     </div>
