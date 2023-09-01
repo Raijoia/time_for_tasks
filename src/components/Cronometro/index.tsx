e43f7fc3ed975173295408a949a49db1e5e9b89a
Raí <megac154@gmail.com>
2023-08-31 21:34:18 -0300

    implement tarefa finish

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -6,40 +6,42 @@ import { tempoParaSegundos } from '../../common/utils/time'
 import { ITarefa } from '../../types/tarefa'
 
 interface Props {
-  selecionado: ITarefa | undefined
+  selecionado: ITarefa | undefined,
+  finalizarTarefa: () => void
 }
 
-export default function Cronometro({ selecionado }: Props) {
-  const [tempo, setTempo] = useState<number>()
-  
-  // useEffect fica esperando alguma mudança, caso houver, ele roda
-  useEffect(() => {
-    if (selecionado?.tempo) {
-      setTempo(tempoParaSegundos(String(selecionado.tempo)))
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
+    const [tempo, setTempo] = useState<number>()
+
+    // useEffect fica esperando alguma mudança, caso houver, ele roda
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(String(selecionado.tempo)))
+        }
+    }, [selecionado])
+
+    // tipando o contador e falando que se caso for undefined, valor dele será 0
+    function regressiva(contador: number = 0) {
+        // setando um tempo para a função ser iniciada
+        setTimeout(() => {
+            // fazendo um loop
+            if (contador > 0) {
+                setTempo(contador - 1)
+                return regressiva(contador - 1)
+            }
+            finalizarTarefa()
+        }, 1000)
     }
-  }, [selecionado])
 
-  // tipando o contador e falando que se caso for undefined, valor dele será 0
-  function regressiva(contador: number = 0) {
-    // setando um tempo para a função ser iniciada
-    setTimeout(() => {
-      // fazendo um loop
-      if(contador > 0) {
-        setTempo(contador - 1);
-        return regressiva(contador - 1);
-      }
-    }, 1000);
-  }
-
-  return (
-    <div className={style.cronometro}>
-      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-      <div className={style.relogioWrapper}>
-        <Relogio tempo={tempo}/>
-      </div>
-      <Botao onClick={() => regressiva(tempo)}>
-        Começar!
-      </Botao>
-    </div>
-  )
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}>
+                Escolha um card e inicie o cronômetro
+            </p>
+            <div className={style.relogioWrapper}>
+                <Relogio tempo={tempo} />
+            </div>
+            <Botao onClick={() => regressiva(tempo)}>Começar!</Botao>
+        </div>
+    )
 }
