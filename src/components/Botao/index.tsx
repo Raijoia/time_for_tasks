52a85654679a96829ab847700b6397cdfb7a89d7
Raí <megac154@gmail.com>
2023-08-31 21:40:00 -0300

    refactor botao for function component

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import style from './Botao.module.scss'
 
+interface Props {
+   children: React.ReactNode,
+   type?: "button" | "submit" | "reset" | undefined,
+   onClick?: () => void
+}
+
 // colocando prop no modo de class, ja colocando o type da props
-class Botao extends React.Component<{ children: string, type?: "button" | "submit" | "reset" | undefined, onClick?: () => void }> {
-  render() {
-    const { type = 'button', onClick } = this.props
-    return (
-      <button className={style.botao} type={type} onClick={onClick}>
-        {this.props.children}
-      </button>
-    )
-  }
+function Botao({ children, type = 'button', onClick }: Props) {
+  return (
+    <button className={style.botao} type={type} onClick={onClick}>
+      {children}
+    </button>
+  )
 }
 
 export default Botao
